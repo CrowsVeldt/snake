41a8a81062ee069230e6fe9f6a9cde934567b6bd
CrowsVeldt <zechy.mahler@gmail.com>
2017-12-25 19:24:58 +0200

    Add spaces to the board

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,44 @@ import React from 'react'
 import styled from 'styled-components'
 
 const App = () => {
+  let board = generateGameBoard()
   return (
-    <GameSpace />
+    <GameBoard>
+      {
+        board.map(row => {
+          return row.map(item => {
+            return <GameSpace />
+          })
+        })
+      }
+    </GameBoard>
+
   )
 }
 
-const GameSpace = styled.div`
-  height: 500px;
-  width: 500px;
+function generateGameBoard () {
+  const board = []
+  let index = 100
+
+  while (index > 0) {
+    board.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    index--
+  }
+  return board
+}
+
+const GameBoard = styled.div`
+  height: 100px;
+  width: 100px;
   background-color: black;
+  display: flex;
+  flex-wrap: wrap;
+`
+
+const GameSpace = styled.div`
+  height: 8px;
+  width: 8px;
+  border: 0.2px solid white;
 `
 
 export default App
