8f36c0f4481116856ee1c23017587ebc5ace9833
CrowsVeldt <zechy.mahler@gmail.com>
2018-01-01 09:41:57 +0200

    add gameLoop

---
+++ src/gameLoop.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux'
+
+let GameLoop = () => {
+  // TODO: Every 60th of a second, dispatch actions to the store to update the game state as appropriate
+}
+GameLoop = connect()(GameLoop)
+
+export default GameLoop
