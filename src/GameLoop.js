6feb43cfe817e193191d4183e688a2d2abc31f54
CrowsVeldt <zechy.mahler@gmail.com>
2018-01-01 10:09:40 +0200

    Add comment to remind me where I left off

--- src/GameLoop.js
+++ src/GameLoop.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux'
 
-let GameLoop = ({store}) => {
-  console.log(store)
+let GameLoop = () => {
+  // TODO: Figure out how to get state from context here
   // TODO: Every 60th of a second, dispatch actions to the store to update the game state as appropriate
   return null
 }
