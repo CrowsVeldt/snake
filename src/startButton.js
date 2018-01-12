3ca5814e520cc5deb3b884d6cb670fac9f0be6ad
CrowsVeldt <zechy.mahler@gmail.com>
2018-01-12 18:26:18 +0200

    add startGame dispatch to startButton

--- src/startButton.js
+++ src/startButton.js
@@ -1,10 +1,12 @@
 import setSnakePosition from './actions/setSnakePosition'
+import startGame from './actions/startGame'
 
 const createStartButton = (dispatch) => {
   const button = document.createElement('button')
   button.innerHTML = 'Start'
   if (dispatch) {
     button.onclick = () => {
+      dispatch(startGame())
       dispatch(setSnakePosition([2, 2]))
     }
   }
