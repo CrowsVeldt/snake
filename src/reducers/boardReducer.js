7a03a7959dcf81b6f80779c5cd291233708db23b
CrowsVeldt <zechy.mahler@gmail.com>
2018-01-12 11:03:14 +0200

    implement setPiecePosition function

--- src/reducers/boardReducer.js
+++ src/reducers/boardReducer.js
@@ -5,4 +5,20 @@ const boardReducer = (state = [], action) => {
   }
 }
 
+export function setPiecePosition (piece, board, position) {
+  return board.map((subArray, index) => {
+    if (index === position[0]) {
+      return subArray.map((item, index2) => {
+        if (index2 === position[1]) {
+          return piece
+        } else {
+          return item
+        }
+      })
+    } else {
+      return subArray
+    }
+  })
+}
+
 export default boardReducer
--- test/reducers/boardReducer.test.js
+++ test/reducers/boardReducer.test.js
@@ -1,9 +1,43 @@
 /* eslint-env jest */
-import boardReducer from '../../src/reducers/boardReducer'
+import boardReducer, { setPiecePosition } from '../../src/reducers/boardReducer'
+
+const validBoardArray = [
+  [1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1]
+]
+
+const testResult = setPiecePosition(1, validBoardArray, [1, 1])
 
 describe('The Board Reducer', () => {
   test('returns the state unchanged by default', () => {
     const testAction = {type: 'TEST'}
     expect(boardReducer({i: 1}, testAction)).toEqual({i: 1})
+  })
+})
+
+describe('setPiecePosition', () => {
+  test('should be a function', () => {
+    expect(typeof setPiecePosition).toEqual('function')
+  })
+
+  test('should throw an error if called without parameters', () => {
+    expect(() => {
+      setPiecePosition()
+    }).toThrow()
+  })
+
+  test('should return an array', () => {
+    expect(Array.isArray(testResult)).toEqual(true)
+  })
+
+  test('should return a changed array', () => {
+    expect(testResult).not.toEqual(validBoardArray)
+  })
+
+  test('the element at "position" shoult equal the "piece" parameter', () => {
+    expect(testResult[1][1]).toEqual(1)
   })
 })
